ef1d95f2d059fa190af1f8b0bf02c87bff13c96e
Manvish-V-M-916 <vmanvish@gmail.com>
2024-12-11 13:01:44 +0530

    Create elif.py

---
+++ elif.py
@@ -0,0 +1,13 @@
+marks = int(input("Enter your marks:"))
+if marks >= 90:
+    print("Grand: A")
+elif marks >= 80:
+    print("Grand: B")
+elif marks >= 75:
+  print("Grand: C")
+elif marks >= 70:
+    print("Grand: D")
+elif marks >= 60:
+    print("Grand: E")
+else:
+    print("Grand: F")
